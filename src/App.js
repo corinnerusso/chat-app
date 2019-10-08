cf430b5ff2440ad74e73c8351689118dbf82f85c
Corinne Russo <corinne.russo@gmail.com>
2019-10-08 17:38:56 +0200

    modif contact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Contact from './components/Contacts';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Contact image ="https://randomuser.me/api/portraits/women/76.jpg"
+               name ="Alain Souchon"
+               
+               online />
+
+      <Contact image = "https://randomuser.me/api/portraits/men/78.jpg"
+               name ="Alex Terieur"
+               offline />
+
+      <Contact image = "https://randomuser.me/api/portraits/women/72.jpg"
+               name ="Amy Molette"
+               
+               online />
+      
     </div>
   );
 }
